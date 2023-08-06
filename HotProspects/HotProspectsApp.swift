0962bdca55998a625221b12a82009b8cf6c10504
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-06 16:20:09 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Kirill Streltsov on 06.08.23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
